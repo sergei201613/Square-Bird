d7d2c2ff888d0bd194833d0c45640f5513cb0a6a
sergei201613 <sgorey@bk.ru>
2020-02-25 19:30:43 +0800

    Create the project

---
+++ Assets/Prefabs/Bird/BirdMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class BirdMovement : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+
+    public LayerMask groundMask;
+
+    private Rigidbody2D _rb;
+    private bool _isGrounded;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.W) && _isGrounded)
+            _rb.AddForce(Vector2.up * jumpForce);
+    }
+}
